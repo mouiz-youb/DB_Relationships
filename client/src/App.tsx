97619228d5178f8d9e9f0c3fa24f0560dbf94511
abdelmouiz <a_youb@estin.dz>
2025-10-07 07:58:36 -1100

    client side is done the real time chat is done the next step is tested a complecated thing in the rell time funciotnnality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Chat from './components/Chat'
 import "./App.css"
 function App() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center flex-col text-6xl text-yellow-600'>
-      hello from react app
+    <div className='w-screen h-screen flex justify-center items-center flex-col '>
+      <h1>Mini Social Media app - Real time chat </h1>
+      <Chat/>
+
     </div>
   )
 }
