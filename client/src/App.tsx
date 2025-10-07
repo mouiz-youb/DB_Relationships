56381a537a505e14f6c20d5a7c01b888578e949e
abdelmouiz <a_youb@estin.dz>
2025-10-07 07:05:44 -1100

    create the client side the next step is to create a simple socket server inside hte server side

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css"
+function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center flex-col text-6xl text-yellow-600'>
+      hello from react app
+    </div>
+  )
+}
+
+export default App
