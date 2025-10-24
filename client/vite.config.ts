5fb76a0384b275642b85caef97b0fde952c6b5f3
abdelmouiz <a_youb@estin.dz>
2025-10-24 06:21:29 -1100

    test 4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/DB_Relationships', // <-- ADD THIS LINE
+  base: '/', // <-- ADD THIS LINE
 })
